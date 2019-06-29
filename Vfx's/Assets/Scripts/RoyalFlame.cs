a3986ebf7c7da687aa7faaf501ac13355a15e12c
Caprice00 <32291691+Caprice00@users.noreply.github.com>
2019-06-30 01:40:32 +0530

    UI and recolors

---
+++ Vfx's/Assets/Scripts/RoyalFlame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class RoyalFlame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int i;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void NextScene()
+    {
+        
+        SceneManager.LoadScene("RoyalFlame");
+    }
+}
